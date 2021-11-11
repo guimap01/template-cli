0ffebf35be0ef8d9d532ea8d03966cac4ae31844
Pedro Monteiro Guimaraes <pedro.guimaraes@ambevtech.com.br>
2021-11-11 13:24:42 -0300

    change variable name

--- src/replaceVariabless/index.js
+++ src/replaceVariabless/index.js
@@ -5,12 +5,12 @@ export function replaceVariables(userOptions) {
   const directory = process.cwd();
   const optionsPackageJson = {
     files: path.resolve(new URL(directory).pathname, 'package.json'),
-    from: /{simples_remote}/g,
+    from: /simples_remote/g,
     to: userOptions.projectName,
   };
   const optionsWebpack = {
     files: path.resolve(new URL(directory).pathname, 'webpack.config.js'),
-    from: /{simple_remote}/g,
+    from: /simple_remote/g,
     to: userOptions.projectName,
   };
   replace.sync(optionsPackageJson);
